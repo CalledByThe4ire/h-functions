c804d56b22089141ed1dd9973b16512fcdba52f7
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-10 18:27:01 +0300

    add solution

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,6 +1,16 @@
 // @flow
 import { length, substr } from './strings';
 
-const isPalindrome = (str: string): boolean => {};
+const isPalindrome = (str: string): boolean => {
+  if (length(str) < 2) {
+    return true;
+  }
+
+  if (substr(str, 0, 1) !== substr(str, length(str) - 1, 1)) {
+    return false;
+  }
+
+  return isPalindrome(substr(str, 1, length(str) - 2));
+};
 
 export default isPalindrome;
