63a89e42aef940be0842cd64973f41b9c6dcf56a
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-11 19:05:45 +0300

    add solution

--- src/substr.js
+++ src/substr.js
@@ -1,6 +1,45 @@
 // @flow
 import { identity, getLength } from './strings';
 
-const substr = (str: string, index: number, length: number): string => {};
+const substr = (
+  str: string = '',
+  index: number = 0,
+  length: number = getLength(str),
+): string => {
+  let result: string = '';
+  let strIndex: number;
+  let strLength: number;
+
+  if (
+    index <= getLength(str) &&
+    index >= 0 &&
+    index + length > getLength(str)
+  ) {
+    strLength = getLength(str);
+  } else if (length < 0) {
+    strLength = 1;
+  } else {
+    strLength = length;
+  }
+
+  if (index < 0) {
+    strIndex = 0;
+  } else {
+    strIndex = index;
+  }
+
+  if (strLength === 0 && strIndex > getLength(str)) {
+    return '';
+  }
+
+  if (strIndex === 0 && strLength === getLength(str)) {
+    return identity(str);
+  }
+
+  for (let i: number = strIndex; i < strLength; i += 1) {
+    result += str[strIndex];
+  }
+  return result;
+};
 
 export default substr;
