6f2bcc1d7df82a83ebdbe1a892600ce11e804e36
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-11 21:43:15 +0300

    add hexlet' solution

--- src/substr.js
+++ src/substr.js
@@ -1,45 +1,29 @@
 // @flow
-import { identity, getLength } from './strings';
+import { getLength } from './strings';
 
-const substr = (
-  str: string = '',
+export default (
+  str: string,
   index: number = 0,
-  length: number = getLength(str),
-): string => {
-  let result: string = '';
-  let strIndex: number;
-  let strLength: number;
+  substrLength: number = getLength(str),
+) => {
+  const length = getLength(str);
 
-  if (
-    index <= getLength(str) &&
-    index >= 0 &&
-    index + length > getLength(str)
-  ) {
-    strLength = getLength(str);
-  } else if (length < 0) {
-    strLength = 1;
-  } else {
-    strLength = length;
-  }
-
-  if (index < 0) {
-    strIndex = 0;
-  } else {
-    strIndex = index;
-  }
-
-  if (strLength === 0 && strIndex > getLength(str)) {
+  if (index >= length) {
     return '';
   }
 
-  if (strIndex === 0 && strLength === getLength(str)) {
-    return identity(str);
+  const currentIndex = index < 0 ? 0 : index;
+  let lastIndex = currentIndex + (substrLength - 1);
+  if (lastIndex + 1 < currentIndex) {
+    lastIndex = currentIndex;
+  } else if (lastIndex > length - 1) {
+    lastIndex = length - 1;
   }
 
-  for (let i: number = strIndex; i < strLength; i += 1) {
-    result += str[strIndex];
+  let result = '';
+  for (let i = currentIndex; i <= lastIndex; i += 1) {
+    result += str[i];
   }
+
   return result;
 };
-
-export default substr;
