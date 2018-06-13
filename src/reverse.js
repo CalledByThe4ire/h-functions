71207028d97856d5b21d2d0ff8e6cd9ed03949f7
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-13 15:31:25 +0300

    add solution

--- src/reverse.js
+++ src/reverse.js
@@ -1,5 +1,13 @@
 // @flow
 
-const reverse = (str: string): string => {};
+const reverse = (str: string): string => {
+  const iter = (index: number, accumulator: string) => {
+    if (str[index] === undefined) {
+      return accumulator;
+    }
+    return iter(index + 1, `${str[index]}${accumulator}`);
+  };
+  return iter(0, '');
+};
 
 export default reverse;
